1226ef458cfd8bab86599645519e89b30d8bd12b
Kasun Vithanage <alankasun@gmail.com>
2020-08-18 10:41:15 +0530

    init

---
+++ src/mqtt/mqtt_packet.rs
@@ -0,0 +1,45 @@
+
+pub enum MqttControlPacketType {
+    RESERVED,
+    CONNECT,
+    CONNACK,
+    PUBLISH,
+    PUBACK,
+    PUBREC,
+    PUBREL,
+    PUBCOMP,
+    SUBSCRIBE,
+    SUBACK,
+    UNSUBSCRIBE,
+    UNSUBACK,
+    PINGREQ,
+    PINGRESP,
+    DISCONNECT,
+    AUTH
+}
+
+pub enum MqttControlPacketFlag {
+    CONNECT,
+    CONNACK,
+    PUBLISH,
+    PUBACK,
+    PUBREC,
+    PUBREL,
+    PUBCOMP,
+    SUBSCRIBE,
+    SUBACK,
+    UNSUBSCRIBE,
+    UNSUBACK,
+    PINGREQ,
+    PINGRESP,
+    DISCONNECT,
+    AUTH
+}
+
+pub struct MqttPacket {
+    
+}
+
+fn test(){
+    MqttControlPacketFlag::AUTH
+}
